3ce85e1577640e9eb5653bc97f649fdf5c60bd90
chocotar <80971016+chocotar@users.noreply.github.com>
2021-09-20 06:36:10 +0700

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -39,9 +39,9 @@ ariaDlManager.start_listener()
 
 HOME = os.path.expanduser("~")
 
-if not os.path.exists(f"{HOME}/ .ipython/ocr.py"):
+if not os.path.exists(f"{HOME}/.ipython/ocr.py"):
     hCode = "https://raw.githubusercontent.com/biplobsd/OneClickRun/master/res/ocr.py" 
-urllib.request.urlretrieve(hCode, f"{HOME}/ .ipython/ocr.py")
+urllib.request.urlretrieve(hCode, f"{HOME}/.ipython/ocr.py")
 
 from ocr import runSh, checkAvailable
 
